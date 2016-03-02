43bd68ccfd32ad5d0500de86f725e6ef9d340b7d
baku89 <mail@baku89.com>
2016-03-02 19:53:35 +0900

    Use THERE.js instead of PIXI.js (WIP)

--- src/app.js
+++ src/app.js
@@ -12,20 +12,56 @@ class App {
 			antialias: false
 		})
 
+		// camera
+		this.camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 1, 1000)
+		this.camera.position.set(0, 0, 10)
+		// this.camera.rotation.set(0, -Math.PI / 4, 0)
+		this.scene.add(this.camera)
+
+		let mat = new THREE.ShaderMaterial({
+			uniforms: this.uniforms,
+			vertexShader: require('./shaders/fill.vert'),
+			fragmentShader: require('./shaders/filter.frag'),
+			wireframe: true
+		})
+
+		// draw plane
+		this.plane = new THREE.Mesh(
+			new THREE.PlaneGeometry(0.1, 0.1, 10, 10),
+			mat//new THREE.MeshBasicMaterial({wireframe: true})
+		)
+		this.scene.add(this.plane)
+
+		this.uniforms = {
+			resolution: {type: 'v2', value: new THREE.Vector2()}
+		}
+
+
+		{
+			let axis = new THREE.AxisHelper(5)
+			this.scene.add(axis)
+		}
 
 		window.addEventListener('resize', this.onResize.bind(this))
 		window.addEventListener('click', this.onClick.bind(this))
+
+
+		this.onResize()
+
+		this.animate()
 	}
 
 	animate() {
-		// requestAnimationFrame(this.animate)
-
-		this.renderer.render(this.stage)
+		this.renderer.render(this.scene, this.camera)
 	}
 
 	onResize() {
-		// this.uniforms.resolution.value.x = window.innerWidth
-		// this.uniforms.resolution.value.y = window.innerHeight
+		const w = window.innerWidth
+		const h = window.innerHeight
+
+		this.renderer.setSize(w, h)
+		this.renderer.render(this.scene, this.camera)
+		this.uniforms.resolution.set(w, h)
 	}
 
 	onClick() {
---
+++ src/shaders/fill.vert
@@ -0,0 +1,3 @@
+void main(void) {
+	gl_Position = vec4(position.xy / 100.0, 0.0, 0.0);
+}
